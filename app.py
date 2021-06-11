8eaea9561f55d62c67db3fbd44f29a84bf55fa73
Shailendra jain <shailendrajain28@gmail.com>
2021-06-11 08:27:02 +0530

    removed wsgi server

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     return "Hello World"
 
 if __name__ == '__main__':
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run( threaded=True, host='0.0.0.0', port=port)
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    port = int(os.environ.get('PORT', 5000))
+    app.run( threaded=True, host='0.0.0.0', port=port)
+    # http_server = WSGIServer(('', 5000), app)
+    # http_server.serve_forever()
