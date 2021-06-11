7e8793e4aa38e67d78c75d9c8f14a6c02e9c8137
Shailendra jain <shailendrajain28@gmail.com>
2021-06-11 08:11:13 +0530

    Added WSGI server

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import os
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 
@@ -8,5 +9,7 @@ def index():
     return "Hello World"
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run( threaded=True, host='0.0.0.0', port=port)
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run( threaded=True, host='0.0.0.0', port=port)
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
